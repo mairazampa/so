057a72ba09ea26e2e73ef4108be45d5041413b14
mairazampa <87398878+mairazampa@users.noreply.github.com>
2022-10-04 13:31:06 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
-class Procesos(models.Model):
+
+class Proceso(models.Model):
     
     nombre = models.CharField(max_length=20)
     arribo = models.IntegerField()
@@ -15,19 +15,24 @@ class Procesos(models.Model):
 
 class Estrategia(models.Model):
     
-    nombre = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50)
     id = models.IntegerField()
     
 
     def __str__(self):
         return self.nombre
 
+class Numero(models.Model):
+    numbero = models.IntegerField()
+    
 class Memoria(models.Model):
     tamano = models.IntegerField()
     id_estrategia = models.ForeignKey(Estrategia, on_delete=models.CASCADE)
     tiempo_carga = models.IntegerField()
     tiempo_liberacion= models.IntegerField()
     tiempo_seleccion = models.IntegerField()
+    id_proceso = models.ForeignKey(Proceso, on_delete=models.CASCADE, related_name = "id_numero")
+    id_numero = models.ForeignKey(Numero, on_delete = models.CASCADE, related_name = "id_proceso")
 
     def __str__(self):
         return self.nombre
